82a24497fe0bd95d9c501ea4ac435774ee6700bb
Alfonso Sánchez-Macián <alfonso@sanchezmacian.com>
2021-09-03 19:59:37 +0200

    Correcting parameters to retrieve the ENG/DE use case results

--- runUseCases.bat
+++ runUseCases.bat
@@ -1,6 +1,6 @@
-python src/testerUseCases.py -m 65536 -n 6000 -s 200 -e 6700 -u 800073 -k 4 -i 100 -f data/Eng_US.Freq.2.txt
-python src/testerUseCases.py -m 65536 -n 6800 -s 50 -e 7200 -u 800073 -k 4 -i 100 -f data/Eng_US.Freq.2.txt
-python src/testerUseCases.py -m 65536 -n 8000 -s 50 -e 12100 -u 800073 -k 4 -i 100 -f data/Eng_US.Freq.2.txt
-python src/testerUseCases.py -m 65536 -n 6000 -s 1000 -e 9100 -u 1711608 -k 4 -i 100 -f data/De.Freq.2.txt
-python src/testerUseCases.py -m 65536 -n 9500 -s 50 -e 9800 -u 1711608 -k 4 -i 100 -f data/De.Freq.2.txt
-python src/testerUseCases.py -m 65536 -n 10000 -s 1000 -e 12100 -u 1711608 -k 4 -i 100 -f data/De.Freq.2.txt
+python src/testerUseCases.py -m 65536 -n 6000 -s 200 -e 6700 -u 1711608 -k 4 -i 100 -f data/De.Freq.2.txt
+python src/testerUseCases.py -m 65536 -n 6800 -s 50 -e 7200 -u 1711608 -k 4 -i 100 -f data/De.Freq.2.txt
+python src/testerUseCases.py -m 65536 -n 8000 -s 1000 -e 12100 -u 1711608 -k 4 -i 100 -f data/De.Freq.2.txt
+python src/testerUseCases.py -m 65536 -n 6000 -s 1000 -e 9100 -u 800073 -k 4 -i 100 -f data/Eng_US.Freq.2.txt
+python src/testerUseCases.py -m 65536 -n 9500 -s 50 -e 9800 -u 800073 -k 4 -i 100 -f data/Eng_US.Freq.2.txt
+python src/testerUseCases.py -m 65536 -n 10000 -s 1000 -e 12100 -u 800073 -k 4 -i 100 -f data/Eng_US.Freq.2.txt
